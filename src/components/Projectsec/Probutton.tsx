5e080fd4ae3724abfb0a99605ab145c2fcdfc295
fateme.dev <155579918+fatemeMohamadian@users.noreply.github.com>
2024-09-21 13:58:20 +0330

    Update Probutton.tsx

--- src/components/Projectsec/Probutton.tsx
+++ src/components/Projectsec/Probutton.tsx
@@ -4,12 +4,12 @@ export default function Probutton() {
     return (
         <div className='flex flex-wrap text-gray-100 font-maintext justify-center items-center w-full lg:w-[50%]'>
             <motion.button
-                whileHover={{ scale: 1.1, backgroundColor:'rgb(1, 143, 70)' }}
+                whileHover={{ scale: 1.1, backgroundColor:'rgb(165, 54, 54)' }}
                 whileTap={{ scale: 0.9 }}
                 transition={{ type: "spring", damping: 10 }}
-                style={{ border: '1px solid rgb(140, 252, 181)',boxShadow:'0 0 50px 10px rgb(1, 143, 70) ', padding: '10px 25px', borderRadius: '10px' }}
+                style={{ border: '1px solid #ffabab',boxShadow:'0 0 50px 10px rgb(101, 33, 16) ', padding: '10px 25px', borderRadius: '10px' }}
             >
-               <a href="https://github.com/fatemeMohamadian?tab=repositories">All Projects <EastIcon style={{color:'rgb(76, 170, 122)', rotate:'-35deg'}}/></a>
+               <a href="https://github.com/fatemeMohamadian?tab=repositories">All Projects <EastIcon style={{color:'#ffabab', rotate:'-35deg'}}/></a>
             </motion.button>
         </div>
     )
